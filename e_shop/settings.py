d5fdf9c94be8c20f0dc7cfbb4db5d9c7fc4a53bd
saidulvai <saidulvaicsebgctub@gmail.com>
2025-09-21 04:57:52 +0600

    added sites

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'shop',
     'allauth',
     'allauth.account',
